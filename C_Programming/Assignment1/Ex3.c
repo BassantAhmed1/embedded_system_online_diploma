6d427850d6107c1ea9bcfdafdb70624760f1c990
BassantAhmed1 <87976366+BassantAhmed1@users.noreply.github.com>
2021-07-31 18:18:58 +0200

    third Ex

---
+++ C_Programming/Assignment1/Ex3.c
@@ -0,0 +1,19 @@
+/*
+ * Ex3.c
+ *
+ *  Created on: Jul 31, 2021
+ *      Author: BassantAhmed
+ */
+#include <stdio.h>
+
+int main()
+{
+	int x,y;
+	printf("Enter two integers:");
+	fflush(stdout);
+	scanf("%d" , &x);
+	scanf("%d" , &y);
+	printf("Sum:%d",x+y);
+	return 0;
+}
+
