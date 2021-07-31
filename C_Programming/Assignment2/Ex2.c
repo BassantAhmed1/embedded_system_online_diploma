927e0ac2e844e427175a742a1b03a094641656b0
BassantAhmed1 <87976366+BassantAhmed1@users.noreply.github.com>
2021-07-31 17:57:13 +0200

    second Ex

---
+++ C_Programming/Assignment2/Ex2.c
@@ -0,0 +1,19 @@
+/*
+ * Ex2.c
+ *
+ *  Created on: Jul 31, 2021
+ *      Author: BassantAhmed
+ */
+#include <stdio.h>
+
+int main()
+{
+	int x;
+	printf("Enter an integer :");
+	fflush(stdout);
+	scanf("%d",&x);
+	printf("You entered:%d \n", x);
+
+	return 0;
+}
+
