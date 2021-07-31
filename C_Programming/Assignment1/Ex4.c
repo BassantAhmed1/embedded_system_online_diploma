97510dc798079a81fdfc19693fe5228db25ae800
BassantAhmed1 <87976366+BassantAhmed1@users.noreply.github.com>
2021-07-31 18:27:09 +0200

    4th Ex

---
+++ C_Programming/Assignment1/Ex4.c
@@ -0,0 +1,18 @@
+/*
+ * Ex4.c
+ *
+ *  Created on: Jul 31, 2021
+ *      Author: BassantAhmed
+ */
+#include <stdio.h>
+
+int main()
+{
+	float x,y;
+	printf("Enter two numbers:");
+	fflush(stdout);
+	scanf("%f %f",&x ,&y);
+	printf("Product:%f", x*y);
+	return 0;
+}
+
