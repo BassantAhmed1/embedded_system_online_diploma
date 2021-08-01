c40a96dc8a57562bd7e2e9ee1e665dd86283f2a2
BassantAhmed1 <87976366+BassantAhmed1@users.noreply.github.com>
2021-08-01 22:27:25 +0200

    7th Ex

---
+++ C_Programming/Assignment1/Ex7.c
@@ -0,0 +1,22 @@
+/*
+ * Ex7.c
+ *
+ *  Created on: Aug 1, 2021
+ *      Author: BassantAhmed
+ */
+#include <stdio.h>
+
+int main()
+{
+	float a,b;
+	printf("Enter value of a:");
+	fflush(stdin);	fflush(stdout);
+	scanf("%f",&b);
+	printf("Enter value of b:");
+	fflush(stdin);	fflush(stdout);
+	scanf("%f",&a);
+	printf("After swapping, value of a =%f \r\n",a);
+	printf("After swapping, value of b =%f\r\n",b);
+
+}
+
