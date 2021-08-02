f999e0ab919b8966c591a3153478a79f41697080
BassantAhmed1 <87976366+BassantAhmed1@users.noreply.github.com>
2021-08-02 11:09:53 +0200

    1st Ex

--- C_Programming/Assignment2/Ex1.c
+++ C_Programming/Assignment2/Ex1.c
@@ -1,1 +1,22 @@
+/*
+ * Ex1.c
+ *
+ *  Created on: Aug 2, 2021
+ *      Author: BassantAhmed
+ */
 
+#include <stdio.h>
+
+int main()
+{
+	int number;
+	printf("Enter an integer you want to check:");
+	fflush(stdin);	fflush(stdout);
+	scanf("%d",&number);
+	if (number%2==0)
+		printf("%d is even \r\n",number);
+	else
+		printf("%d is odd ",number);
+
+	return 0;
+}
