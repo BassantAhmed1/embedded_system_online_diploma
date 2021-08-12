74432870c6447c1c809fc40e245359c1ebab3d66
BassantAhmed1 <87976366+BassantAhmed1@users.noreply.github.com>
2021-08-12 14:51:36 +0200

    prime numbers between 2 intervals

---
+++ C_Programming/Assignment4/Ex1.c
@@ -0,0 +1,34 @@
+/*
+ * Ex1.c
+ *
+ *  Created on: Aug 12, 2021
+ *      Author: BassantAhmed
+ */
+#include <stdio.h>
+
+
+//void prime_num(int num1, int num2);
+
+int main ()
+{
+
+	int num1,num2;
+	printf("Enter two numbers (intervals):");
+	fflush(stdin);	fflush(stdout);
+	scanf("%d %d",&num1,&num2);
+	printf("prime numbers between %d and %d are: ",num1,num2);
+	prime_num(num1,num2);
+	return 0;
+
+}
+void prime_num(int num1, int num2)
+{
+	for (; num1<=num2 ;num1++)
+		{
+			if (num1==11)
+				printf("%d ",num1);
+			else if(!((num1%2==0)||(num1%3==0) || (num1%5==0) || (num1%7==0) || (num1%11==0)))
+				printf("%d ",num1);
+
+		}
+}
