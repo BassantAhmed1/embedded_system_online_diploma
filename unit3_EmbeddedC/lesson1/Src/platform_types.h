0f35e74836f4c075bdea0835d9bca6ac065ff287
BassantAhmed1 <87976366+BassantAhmed1@users.noreply.github.com>
2021-09-27 23:17:54 +0200

    adding code

---
+++ unit3_EmbeddedC/lesson1/Src/platform_types.h
@@ -0,0 +1,13 @@
+/*
+ * platform_types.h
+ *
+ *  Created on: Sep 19, 2021
+ *      Author: MAK
+ */
+
+#ifndef PLATFORM_TYPES_H_
+#define PLATFORM_TYPES_H_
+#endif /* PLATFORM_TYPES_H_ */
+
+typedef _bool			boolean;
+typedef int8_t			sint8;
